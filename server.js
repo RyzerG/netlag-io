df6786cfd7f971429599f3262479aabae56d4902
RyzerG <rohan.singh1218@gmail.com>
2026-06-30 21:12:55 -0400

    Update default server port to 9050 in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const https = require('https');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9050;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
